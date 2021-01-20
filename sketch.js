e78fd255a734b8f2e51b5d38d10f08c7ce4af3c5
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-01-20 15:20:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,16 +3,19 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var engine, world;
 var ground;
 
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+   
+  engine = Engine.create();
+  world = engine.world;
 
   ground = new Ground(200, 100, 60, 50);
 }
 
 function draw() {
   background(255,255,255);  
-  drawSprites();
+  ground.display();
 }
